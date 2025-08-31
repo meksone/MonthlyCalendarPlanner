7c3507a615f7fe51b731fcfb8ecd2d08c6f4f653
meksONE <meksone@gmail.com>
2025-08-31 19:14:32 +0200

    feat: aggiungere file di traduzione in italiano per il plugin mk-monthly-calendar-planner

---
+++ mk-monthly-calendar-planner/assets/languages/mk-monthly-calendar-planner-it_IT.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'mk-monthly-calendar-planner','report-msgid-bugs-to'=>'','pot-creation-date'=>'2025-08-31 16:57+0000','po-revision-date'=>'2025-08-31 16:58+0000','last-translator'=>'','language-team'=>'Italiano','language'=>'it_IT','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.8.0; wp-6.8.2; php-8.3.6','x-domain'=>'mk-monthly-calendar-planner','messages'=>[]];
